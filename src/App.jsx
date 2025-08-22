41290b301c236ae5da2ccc0ea16ba8ae23021222
Jaswanth5996 <jaswanth.140760@gmail.com>
2025-08-22 22:11:16 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Login from '../components/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+    <Login />
+    </div>
+  )
+}
+
+export default App;
